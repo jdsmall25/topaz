caf890e625d98b3410f38d7d7632650ef9afd3dd
jdsmall25 <59858074+jdsmall25@users.noreply.github.com>
2020-06-04 04:59:12 +0000

    Adds setting to always allow chocobo rentals without a license or lvl requirement (#10)

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -156,3 +156,4 @@ FORCE_SPAWN_QM_RESET_TIME = 300 -- Number of seconds the ??? remains hidden for 
 DISABLE_LOTTERY_NM_SYSTEM = 1 -- Set to 1 to disable the lottery system for NMs and have them spawn like normal mobs.
 ALWAYS_GRANT_NATION_RING = 1 -- Set to 1 to always grant a nation ring regardless of race.
 STARTING_WARP_RING = 1 -- Set to 1 to grant a warp ring at character creation.
+ALWAYS_ALLOW_CHOCOBO_RENTAL = 1 -- Set to 1 to always allow a chocobo rental without a license or any level.
