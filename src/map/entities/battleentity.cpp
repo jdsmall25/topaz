d2762e2cd093c89b8ec913d38cae6eb0b92aa9b2
jdsmall25 <github@foreversmall.com>
2020-08-11 02:47:39 -0400

    Syntax fixes

--- src/map/entities/battleentity.cpp
+++ src/map/entities/battleentity.cpp
@@ -1669,10 +1669,10 @@ bool CBattleEntity::OnAttack(CAttackState& state, action_t& action)
                 }
             }
 
-            if (attack.IsCovered && this->objecttype == TYPE_MOB && actionTarget.param >= 0)
+            if (attack.IsCovered && this->objtype == TYPE_MOB && actionTarget.param >= 0)
             {
-                int32 newCE = this.getCE(PTarget) + 200;
-                printf(newCE);
+                int32 newCE = this->getCE(PTarget) + 200;
+                printf(to_string(newCE));
                 this->PEnmityContainer->SetCE(PTarget, newCE);
                 this->PEnmityContainer->LowerEnmityByPercent(OriginalTarget, 10, nullptr);
             }
